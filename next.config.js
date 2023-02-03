4207b656c747dd3b528f151f1aac5f74df547dc8
Rohit Kumar Verma <rohitkverma77@gmail.com>
2023-02-04 00:14:38 +0530

    cors reimagined again

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: "*",
-        destination: "*",
-      },
-    ];
-  },
   images: {
     remotePatterns: [
       {
