0ce52ab5ccddb7372b848f7e86748df062806d6f
Rohit Kumar Verma <rohitkverma77@gmail.com>
2023-02-03 22:42:15 +0530

    final commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
+import Link from "next/link";
+import Search from "./search";
 
 export default function Home() {
   return (
@@ -11,8 +13,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <a href="/search"> Go to search</a>
-        <a href="/newest"> Go to newest</a>
+        <Link href="/search">Go to search</Link>
+        <Link href="/newest"> Go to newest</Link>
       </main>
     </>
   );
